287cba7c18748e8238e3385a2ab0c095b1b49b85
HKhawaja <mkhawaja19@amherst.edu>
2018-12-06 14:06:57 -0500

    Begin work on parsing graph

--- mainp4.py
+++ mainp4.py
@@ -6,12 +6,36 @@ from sklearn.svm import SVC
 # SVC is for Support Vector Classifier -- we called it SVM in class
 from sklearn.tree import DecisionTreeClassifier
 
-if __name__ == "__main__":
-    assert 1 / 2 == 0.5, "Are you sure you're using python 3?"
-    #tr_file = open("posts_train.txt", "r")
-    #tr = tr_file.read().split(',')
-    #print(tr)
+def clean_up_graph(ids):
+    graph = loadtxt("graph.txt", comments='#', delimiter="\t", unpack=False)
+    """
+    want to remove from graph:
+    1) any entries where either id is an id not found in our original list of posts
+    """
+    print('cleaning up graph')
+    for i in range(len(graph)-1, -1, -1):
+        if graph[i][0] not in ids or graph[i][1] not in ids:
+            graph = np.delete(graph, i, 0)
+    print(graph)
 
+    # create list of friend ids
+    friend_list = {}
+    for i in range(len(graph) -1, -1, -1):
+        source = graph[i][0]
+        dest = graph[i][1]
+        if source not in friend_list:
+            dests = [dest]
+            friend_list[source] = dests
+        else:
+            dests = friend_list[source]
+            dests.append(dest)
+            friend_list[source] = dests
+
+    # find median latitude and longitude of each id's 'friends'
+    median_lat_lng = {}
+
+
+def posts_cleanup():
     tr_init = loadtxt("posts_train.txt", comments="#", delimiter=",", unpack=False)
 
     # there are 7 variables
@@ -30,6 +54,10 @@ if __name__ == "__main__":
 
     te_init = loadtxt("posts_test.txt", comments='#', delimiter=",", unpack=False)
 
+    """
+    we would need to get the user id too right, in order to correlate that user
+    to his social graph?
+    """
     X_te = np.empty((len(te_init), 4))
     for a in range(4):
         temp = te_init[:, a+1]
@@ -46,3 +74,15 @@ if __name__ == "__main__":
         y_tr = np.delete(y_tr, k, 0)
         X_tr = np.delete(X_tr, k, 0)
     #Now there are no elements left on null island
+
+    ids_lat_lng = np.empty((len(y_tr), 3))
+    ids_lat_lng[0] =
+    return set(tr_init[:,0])
+
+
+if __name__ == "__main__":
+    assert 1 / 2 == 0.5, "Are you sure you're using python 3?"
+    #tr_file = open("posts_train.txt", "r")
+    #tr = tr_file.read().split(',')
+    #print(tr)
+    clean_up_graph(posts_cleanup())
