ece05f7c2280d4cc1bb5d8414dc4e956869376f6
jcampbell19 <jcampbell19@amherst.edu>
2018-12-09 21:54:25 -0500

    removed bagged regression

--- learners.py
+++ learners.py
@@ -2,13 +2,13 @@ import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import GradientBoostingRegressor
 
-def linearRegressor(tr_X, tr_y, te_X):  # Code to play around with for performing a linear regression
+def linearRegressor(tr_X, tr_y, te_X):  # Code for performing a linear regressor
     linearRegression = LinearRegression(normalize=True)  # creates a linear regression object
     linear_tr = linearRegression.fit(tr_X, tr_y)
     preds = linear_tr.predict(te_X)
     return preds
 
-def gradientBooster(tr_X, tr_y, te_X):
+def gradientBooster(tr_X, tr_y, te_X): #Code for performing a gradient boosting regressor
     GradientRegression = GradientBoostingRegressor(max_depth=5)
     gradientFit = GradientRegression.fit(X=tr_X, y=tr_y, sample_weight=None)
     gradient_preds = gradientFit.predict(te_X)
